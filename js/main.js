a98086ff0ac0861cd8147d89fc340a61c4357251
avilapro <mejiasavilaeduardo@gmail.com>
2023-01-26 13:48:08 -0400

    clase 1

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+console.warn("La pagina se ha cargado");
+// console.log(Epale);
+
+/*Declaracion de variables con diferentes tipos de datos
+*/
+
+//Numericas
+var edad = 34;
+let anio = 1988;
+const sueldo = 1523.14;
+
+let sinDeclarar;
+
+//Alfabeticas
+var nombre = "Eduardo";
+let apellido = "Mejias";
+const cargo = "profesor";
+
+//Booleanas
+var casado = true;
+let millonario = false;
+const trabajador = true;
+
+//Presentacion de los valores y sus tipos en la consola
+console.log(sinDeclarar);
+
+console.log("VARIABLES NUMERICAS");
+console.log("Edad: " + edad);
+console.log(typeof(edad));
+console.log("Anio: " + anio);
+console.log(typeof(anio));
+console.log("Sueldo: " + sueldo);
+console.log(typeof(sueldo));
+
+console.log("VARIABLES ALFABETICAS");
+console.log("Nombre: " + nombre);
+console.log(typeof(nombre));
+console.log("Apellido: " + apellido);
+console.log(typeof(apellido));
+console.log("Cargo: " + cargo);
+console.log(typeof(cargo));
+
+console.log("VARIABLES Booleanas");
+console.log("Casado: " + casado);
+console.log(typeof(casado));
+console.log("Millonario: " + millonario);
+console.log(typeof(millonario));
+console.log("Trabajador: " + trabajador);
+console.log(typeof(trabajador));
---
+++ js/preload.js
@@ -0,0 +1,1 @@
+console.info("Esta a punto de iniciar la pagina");
